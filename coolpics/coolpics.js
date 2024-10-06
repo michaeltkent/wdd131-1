3dbd5f91f8282b2fde089d65ff6d73dca22755bb
michae303 <156228810+michae303@users.noreply.github.com>
2024-10-05 19:17:24 -0600

    webite

---
+++ coolpics/coolpics.js
@@ -0,0 +1,6 @@
+
+document.querySelector('.menu-button').addEventListener('click', () => {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
+  });
+  
