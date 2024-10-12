11cdc80b274493d60619601ebcd0571b4ff5ee13
michae303 <156228810+michae303@users.noreply.github.com>
2024-10-11 22:52:01 -0600

    finished part 2

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,6 +1,45 @@
 
-document.querySelector('.menu-button').addEventListener('click', () => {
+const menuButton = document.querySelector('.menu-button');
+menuButton.addEventListener('click', () => {
     const navLinks = document.querySelector('.nav-links');
-    navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
-  });
-  
+    navLinks.classList.toggle('hide');
+});
+
+
+function handleResize() {
+  const navLinks = document.querySelector('.nav-links');
+  if (window.innerWidth > 1000) {
+    navLinks.classList.remove('hide');
+  } else {
+    navLinks.classList.add('hide');
+  }
+}
+
+handleResize(); 
+window.addEventListener('resize', handleResize);
+
+
+function viewerTemplate(pic, alt) {
+  return `<div class="viewer">
+    <button class="close-viewer">X</button>
+    <img src="${pic}" alt="${alt}">
+  </div>`;
+}
+
+function viewHandler(event) {
+  const imgElement = event.target;
+  const imgSrc = imgElement.src.replace('-sm', '-full'); 
+  const altText = imgElement.alt;
+  document.body.insertAdjacentHTML('afterbegin', viewerTemplate(imgSrc, altText));
+
+  const closeButton = document.querySelector('.close-viewer');
+  closeButton.addEventListener('click', closeViewer);
+}
+
+function closeViewer() {
+  const viewer = document.querySelector('.viewer');
+  viewer.remove(); 
+}
+
+
+document.querySelector('.gallery').addEventListener('click', viewHandler);
